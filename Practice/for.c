b7570b47e1919e2ba366512f00d019d269e1212d
Samaksh Mittal <samakshmittal826@gmail.com>
2023-11-13 21:36:20 +0530

    Create for.c

---
+++ Practice/for.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int i;
+   /*for(i=0; i<5; i++){
+        printf("%d\n", i);
+    }
+    for(i=8; i<5; i++){
+        printf("%d\n", i);
+    }
+    for(i=0; i=6; i++){
+        printf("%d\n", i);
+    }
+    for(i>9; i=6; i++){
+        printf("%d\n", i);
+    }
+    for(i<9; i=6; i++){
+        printf("%d\n", i);
+    }
+    for(i++; i>6; i=8){
+        printf("%d\n", i);
+    }*/
+    for(i=0; i<5; ++i){
+        printf("%d\n", i);
+    }
+}
