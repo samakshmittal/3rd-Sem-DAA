4baf2baa4cf38b13d6534c572b583459a86d1ea7
Samaksh Mittal <samakshmittal826@gmail.com>
2023-11-16 11:18:37 +0530

    Create activity_selection.c

---
+++ Greedy Methods/activity_selection.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct activity{
+    int number;        // Number of activity
+    int start;         // Start time of activity
+    int finish;        // Finish time of activity
+};
+int main(){
+    struct activity *old=NULL;
+    int flag=0;
+    while(flag==0){
+        printf("Enter the number of activity : ");
+        scanf("%d", &old->number);
+        printf("Enter the start time of activity : ");
+        scanf("%d", &old->start);
+        printf("Enter the finish time of activity : ");
+        scanf("%d", &old->finish);
+        struct activity *new=malloc()
+    }
+}
