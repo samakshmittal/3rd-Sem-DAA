9a75b568f963f29d8506c6feb5319071fc070361
Samaksh Mittal <samakshmittal826@gmail.com>
2023-12-23 17:34:03 +0530

    Update tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,17 +1,101 @@
+// C program for Dijkstra's single source shortest path
+// algorithm. The program is for adjacency matrix
+// representation of the graph
+
+#include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-int main() {
-int pipe_fd[2]; pid_t pid;
-char message[] = "Hello, child process!\n"; char read_buffer[100];
-pipe(pipe_fd); pid = fork(); if (pid == 0) {
-close(pipe_fd[1]);
-read(pipe_fd[0], read_buffer, sizeof(read_buffer)); printf("Child Process: Received data from parent: %s",
-read_buffer);
-close(pipe_fd[0]);
-} else {
-close(pipe_fd[0]);
-write(pipe_fd[1], message, sizeof(message)); close(pipe_fd[1]);
+
+// Number of vertices in the graph
+#define V 9
+
+// A utility function to find the vertex with minimum
+// distance value, from the set of vertices not yet included
+// in shortest path tree
+int minDistance(int dist[], bool sptSet[])
+{
+	// Initialize min value
+	int min = INT_MAX, min_index;
+
+	for (int v = 0; v < V; v++)
+		if (sptSet[v] == false && dist[v] <= min)
+			min = dist[v], min_index = v;
+
+	return min_index;
 }
-return 0; 
+
+// A utility function to print the constructed distance
+// array
+void printSolution(int dist[])
+{
+	printf("Vertex \t\t Distance from Source\n");
+	for (int i = 0; i < V; i++)
+		printf("%d \t\t\t\t %d\n", i, dist[i]);
+}
+
+// Function that implements Dijkstra's single source
+// shortest path algorithm for a graph represented using
+// adjacency matrix representation
+void dijkstra(int graph[V][V], int src)
+{
+	int dist[V]; // The output array. dist[i] will hold the
+				// shortest
+	// distance from src to i
+
+	bool sptSet[V]; // sptSet[i] will be true if vertex i is
+					// included in shortest
+	// path tree or shortest distance from src to i is
+	// finalized
+
+	// Initialize all distances as INFINITE and stpSet[] as
+	// false
+	for (int i = 0; i < V; i++)
+		dist[i] = INT_MAX, sptSet[i] = false;
+
+	// Distance of source vertex from itself is always 0
+	dist[src] = 0;
+
+	// Find shortest path for all vertices
+	for (int count = 0; count < V - 1; count++) {
+		// Pick the minimum distance vertex from the set of
+		// vertices not yet processed. u is always equal to
+		// src in the first iteration.
+		int u = minDistance(dist, sptSet);
+
+		// Mark the picked vertex as processed
+		sptSet[u] = true;
+
+		// Update dist value of the adjacent vertices of the
+		// picked vertex.
+		for (int v = 0; v < V; v++)
+
+			// Update dist[v] only if is not in sptSet,
+			// there is an edge from u to v, and total
+			// weight of path from src to v through u is
+			// smaller than current value of dist[v]
+			if (!sptSet[v] && graph[u][v]
+				&& dist[u] != INT_MAX
+				&& dist[u] + graph[u][v] < dist[v])
+				dist[v] = dist[u] + graph[u][v];
+	}
+
+	// print the constructed distance array
+	printSolution(dist);
+}
+
+// driver's code
+int main()
+{
+	/* Let us create the example graph discussed above */
+	int graph[V][V] = { {0, 1, 0, 6, 7, 0 },
+						{ 0, 0, 1, 4, 0, 0},
+						{ 0, 0, 0, 2, 0, 1 },
+						{ 0, 0, 0, 0, 3, 2 },
+						{ 0, 0, 0, 0, 0, 2},
+						{ 0, 0, 0, 0, 0, 0 } };
+
+	// Function call
+	dijkstra(graph, 0);
+
+	return 0;
 }
