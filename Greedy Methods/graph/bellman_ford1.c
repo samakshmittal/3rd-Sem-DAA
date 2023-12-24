6587771a4b617c89b11c5a991eed78b03d27f59c
Samaksh Mittal <samakshmittal826@gmail.com>
2023-12-24 16:19:36 +0530

    Update bellman_ford1.c

--- Greedy Methods/graph/bellman_ford1.c
+++ Greedy Methods/graph/bellman_ford1.c
@@ -26,7 +26,7 @@ void bellman(int graph[][3], int src) {
     dist[src] = 0;
     for (int i = 0; i < V - 1; i++) {
         for (int j = 0; j < 8; j++) {
-            if (dist[graph[j][0]] != INT_MAX && dist[graph[j][0]] + graph[j][2] < dist[graph[j][1]]) {
+            if (dist[graph[i][j]] != INT_MAX && dist[graph[i][j]] + graph[j][2] < dist[graph[j][1]]) {
                 dist[graph[j][1]] = dist[graph[j][0]] + graph[j][2];
             }
         }
