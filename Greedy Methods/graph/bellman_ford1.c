8071ffc4abfaa62040f27e747e3980cb5c6845e1
Samaksh Mittal <samakshmittal826@gmail.com>
2023-12-24 12:47:29 +0530

    Create bellman_ford1.c

---
+++ Greedy Methods/graph/bellman_ford1.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+int V;
+// int minDistance(int dist[], int sptSet[]) {
+//     int min = INT_MAX, min_index;
+//     for (int v = 0; v < V; v++) {
+//         if (!sptSet[v] && dist[v] < min) {
+//             min = dist[v];
+//             min_index = v;
+//         }
+//     }
+//     return min_index;
+// }
+// void printSolution(int dist[]) {
+//     printf("Vertex   Distance from Source\n");
+//     for (int i = 0; i < V; i++) {
+//         printf("%d \t\t %d\n", i, dist[i]);
+//     }
+// }
+void bellman(int graph[][], int src) {
+    int dist[V];
+    for (int i = 0; i < V; i++) {
+        dist[i] = INT_MAX;
+    }
+    dist[src] = 0;
+    for (int i = 0; i < V - 1; i++) {
+        for (int j = 0; j < V*V; j++) {
+            if (dist[graph[j][0]] != INT_MAX && dist[graph[j][0]] + graph[j][2] < dist[graph[j][1]]) {
+                dist[graph[j][1]] = dist[graph[j][0]] + graph[j][2];
+            }
+        }
+    }
+    for(int i=0; i<V; i++){
+        int x=graph[i][0];
+        int y=graph[i][1];
+        int weight=graph[i][2];
+        if(dist[x]!=INT_MAX && dist[x]+weight<dist[y]){
+            printf("\nContains negative cycle\n");
+        }
+    }
+    // printSolution(dist);
+    printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < V; i++) {
+        printf("%d \t\t %d\n", i, dist[i]);
+    }
+}
+int main() {
+    //int **graph=NULL;
+    char choice='y', choice1='y';
+    int i, j, start=0, end=0;
+    unsigned dist;
+    V=8;
+    // while(choice=='y' || choice=='Y'){
+    //     int **temp_graph = malloc((V + 1) * sizeof(int));
+    //     if (temp_graph == NULL) {
+    //         exit(1);
+    //     }
+    //     for (i = 0; i < V + 1; i++) {
+    //         temp_graph[i] = malloc((V + 1) * sizeof(int));
+    //         if (temp_graph[i] == NULL) {
+    //             exit(1);
+    //         }
+    //     }
+    //     for (i = 0; i < V; i++) {
+    //         for (j = 0; j < V; j++){
+    //             temp_graph[i][j] = graph[i][j];
+    //         }
+    //     }
+    //     printf("Number of vertices available till now are %d\n", V+1);
+    //     V++;
+    //     for(i=0; i<V; i++){
+    //         temp_graph[V-1][i]=0;
+    //         temp_graph[i][V-1]=0;
+    //     }
+    //     for (i = 0; i < V; i++) {
+    //         for (j = 0; j < V; j++){
+    //             printf("%d ", temp_graph[i][j]);
+    //         }
+    //         printf("\n");
+    //     }
+    //     choice1='y';
+    //     while((choice1=='y' || choice1=='Y') && V!=1){
+    //         do{
+    //             printf("Enter the starting vertex index : ");
+    //             scanf("%d", &start);
+    //         }while(start>V);
+    //         do{
+    //             printf("Enter the ending vertex index : ");
+    //             scanf("%d", &end);
+    //         }while(end>V);
+    //         printf("Enter distance between them : ");
+    //         scanf("%u", &dist);
+    //         temp_graph[start][end]=dist;
+    //         printf("Do you want to enter new distance with current vertices (Y/N) : ");
+    //         scanf("%s", &choice1);
+    //     }
+    //     graph = temp_graph;
+    //     for (i = 0; i < V; i++) {
+    //         for (j = 0; j < V; j++){
+    //             printf("%d ", graph[i][j]);
+    //         }
+    //         printf("\n");
+    //     }
+    //     printf("Do you want to enter new vertex (Y/N) : ");
+    //     scanf("%s", &choice);
+    // }
+    int graph[V][V] = {
+        {0, 1, -1, 0, 0, 0},
+        {0, 2, 4, 0, 0, 0},
+        {1, 2, 3, 0, 0, 0},
+        {1, 3, 2, 0, 0, 0},
+        {1, 4, 2, 0, 0, 0},
+        {3, 2, 5, 0, 0, 0},
+        {3, 1, 1, 0, 0, 0},
+        {4, 3, -3, 0, 0, 0}
+    };
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++){
+            printf("%d ", graph[i][j]);
+        }
+        printf("\n");
+    }
+    int source;
+    printf("Enter the source node (0 to %d): ", V - 1);
+    scanf("%d", &source);
+    if (source < 0 ||   source >= V) {
+        printf("Invalid source node. Please enter a valid source node (0 to %d).\n", V - 1);
+        return 0;
+    }
+    bellman(graph, source);
+    return 0;
+}
